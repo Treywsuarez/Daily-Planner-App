609928741991259ec5d188755356c595b4da73b0
Treysuarez <treyxsuarez@gmail.com>
2023-02-14 00:48:20 +0000

    added save message with timeout

--- starter/index.js
+++ starter/index.js
@@ -4,11 +4,16 @@ $(document).ready(function () {
     let today = moment().format("dddd Do MMMM, YYYY");
     $("#currentDay").text(today);
 
-     // Define an array to store the time values for each hour
-     let hours = [];
+    // Define an array to store the time values for each hour
+    let hours = [];
 
-     // Loop through the hours from 9am to 5pm and format them
-     for (let i = 9; i <= 17; i++) {
-         let hour = (i < 12 ? i + "am" : (i === 12 ? i + "pm" : (i - 12) + "pm"));
-         hours.push(hour);
-     }
+    // Loop through the hours from 9am to 5pm and format them
+    for (let i = 9; i <= 17; i++) {
+        let hour = (i < 12 ? i + "am" : (i === 12 ? i + "pm" : (i - 12) + "pm"));
+        hours.push(hour);
+    }
+
+    // Declare a variable to store the timeout for the save message
+    let timeout;
+    // Create a save message element
+    let saveMessage = $("<p>").addClass("message").text("Your actvity has been saved");
